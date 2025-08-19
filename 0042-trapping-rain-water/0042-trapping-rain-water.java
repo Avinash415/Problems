b9c044d56d1dd1f17cbc9ccb9931d7d7c482b1e9
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2025-08-19 20:18:59 +0530

    Time: 1 ms (61.67%), Space: 46.4 MB (59.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,58 +1,20 @@
 class Solution {
-    public int trap(int[] height) {
-         int n=height.length;
-        // Calculate left max boundary  --Array
-        // int leftMax[] = new int[n];
-        // leftMax[0]=height[0];
-
-        // for(int i=1; i<n; i++){
-        //     leftMax[i]=Math.max(height[i], leftMax[i-1]);
-        // }
-
-        // // calculate RightMax  boundary  ---Array
-        // int rightMax[]=new int[n];
-        // rightMax[n-1]=height[n-2];
-
-        // for(int i=n-2; i>=0; i--){
-        //     rightMax[i]=Math.max(height[i], rightMax[i+1]);
-        // }
-
-        // int trappedWater=0;
-
-        // //loop
-        // for(int i=0; i<n; i++){
-        //     // WaterLevel = min(leftMax bound , rightMax bound)
-
-        //     int waterLevel=Math.min(leftMax[i], rightMax[i]);
-
-        //     //trappedWater = waterLevel - height[i]
-        //     trappedWater=waterLevel-height[i];
-                
-        // }
-        // return trappedWater;
-          int l=0;
-       int r=n-1;
-       int lmax=0; int rmax=0;int ans=0;
-       while(l<=r)
-       {
-           if(height[l]<=height[r])
-           {
-               if(height[l]<=lmax)
-               ans+=lmax-height[l];
-               else
-               lmax=height[l];
-               l++;
-           }
-           else
-           {
-               if(height[r]<=rmax)
-               ans+=rmax-height[r];
-               else
-               rmax=height[r];
-               r--;
-           }
-       }
-       return ans;
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int[] lmax = new int[n];
+        lmax[0] = arr[0];
+        for (int i = 1; i < n; i++) 
+            lmax[i] = Math.max(lmax[i - 1], arr[i]);
         
+        int[] rmax = new int[n];
+        rmax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+            rmax[i] = Math.max(rmax[i + 1], arr[i]);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) 
+            ans += Math.min(lmax[i], rmax[i]) - arr[i];
+
+        return ans; 
     }
 }
