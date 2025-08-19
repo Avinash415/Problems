6dc27a506069e91ca0f660964c50cef771f965c8
Avinash Kumar <130858842+Avinash415@users.noreply.github.com>
2025-08-19 20:22:08 +0530

    Time: 1 ms (61.67%), Space: 47.1 MB (8.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,19 +1,19 @@
 class Solution {
     public int trap(int[] arr) {
-        int n = arr.length;
-        int[] lmax = new int[n];
-        lmax[0] = arr[0];
+        int l = 0, r = arr.length - 1;
+        int lmax = 0, rmax = 0, ans = 0;
 
-        // Compute left max array
-        for (int i = 1; i < n; i++) 
-            lmax[i] = Math.max(lmax[i - 1], arr[i]);
+        while (l < r) {
+            lmax = Math.max(lmax, arr[l]);
+            rmax = Math.max(rmax, arr[r]);
 
-        int rmax = arr[n - 1], ans = 0;
-
-        // Compute trapped water while updating right max dynamically
-        for (int i = n - 1; i >= 0; i--) {
-            rmax = Math.max(rmax, arr[i]);
-            ans += Math.min(lmax[i], rmax) - arr[i];
+            if (lmax < rmax) {
+                ans += lmax - arr[l];
+                l++;
+            } else {
+                ans += rmax - arr[r];
+                r--;
+            }
         }
         return ans;
     }
